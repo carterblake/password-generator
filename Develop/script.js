b889ea421c9c3f04d1f87e7a50bc89c849eda0c0
carterblake <carterblake17@gmail.com>
2022-11-10 21:42:08 -0600

    added readme

--- Develop/script.js
+++ Develop/script.js
@@ -3,41 +3,40 @@ const lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const syms =["\"", "\'", "\`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", "|", "\\", ":", ";", "<", ">", ",", ".", "/", "?"]
 
-//var charPool = [].concat(upperCaseLetters, lowerCaseLetters); this is how i will combine arrays and create the character pool to randomize
+console.log("greetings grader");
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
+//function that generates password
 function generatePassword() {
   var charPool = [];
   var passwordLength = prompt("Enter your desired password length (between 8-128 characters");
-  if (passwordLength >= 8 && passwordLength <= 128) {
+  if (passwordLength >= 8 && passwordLength <= 128) { //checks for password length
     var hasUpper = confirm("Would you like upper case letters?");
-    if (hasUpper) {var charPool = charPool.concat(upperCaseLetters);}
+    if (hasUpper) {var charPool = charPool.concat(upperCaseLetters);} //adds uppercase letters to pool of characters
     var hasLower = confirm("Would you like lower case letters?");
-    if (hasLower) {var charPool = charPool.concat(lowerCaseLetters);}
+    if (hasLower) {var charPool = charPool.concat(lowerCaseLetters);} //adds lowercase letters to pool of characters
     var hasNums = confirm("Would you like numbers?");
-    if (hasNums) {var charPool = charPool.concat(nums);}
+    if (hasNums) {var charPool = charPool.concat(nums);} //adds numbers (0-9) to pool of characters
     var hasSyms = confirm("Would you like symbols?");
-    if (hasSyms) {var charPool = charPool.concat(syms);}
-    if (!hasUpper && !hasLower && !hasNums && !hasSyms) {
+    if (hasSyms) {var charPool = charPool.concat(syms);} //adds symbols to pool of characters
+    if (!hasUpper && !hasLower && !hasNums && !hasSyms) { //this is a check to see if the user has selected at least one character type
       alert("please select at least one value");
-      return "you shall not pass(word)"
+      return "you shall not pass(word)" //i think i'm funny
     }
     
+    //creates and returns a random password of desired parameters
     let thePassword = "";
 
     for (var i = 1; i <= passwordLength; i ++) {
-      console.log(i);
-      var randomPool = charPool.sort(() => Math.random() - 0.5);
-      console.log(randomPool[0]);
+      var randomPool = charPool.sort(() => Math.random() - 0.5); //this will randomize the character pool every time it loops
       thePassword = thePassword + randomPool[0];
     }
 
     return thePassword;
 
-  } else {
+  } else { //occurs if user does not input a valid number
     alert("please enter a number between 8 and 128");
   }
 }
